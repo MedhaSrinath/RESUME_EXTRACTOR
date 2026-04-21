ef78cba9f7a2d3c84f672a068bdc68d138bb200a
Medha Srinath <medhabengaluru@gmail.com>
2026-04-22 01:04:06 +0530

    Logic developed

--- resume_extractor.py
+++ resume_extractor.py
@@ -1,4 +1,9 @@
 print("Resume Skills Extractor")
 text = input("Paste your resume text: ")
-skills = ["Python", "sql", "java", "machine learning", "c++"]
-print("Skills I know:", skills)
+skills = ["Python", "SQL", "Java", "Machine Learning", "C++", "HTML", "CSS"]
+found_skills = []
+for skill in skills:
+    if skill.lower() in text.lower():
+        found_skills.append(skill)
+
+print("Skills found:", ",".join(found_skills))
